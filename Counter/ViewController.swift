081dacd1885da56d83f298f05e92723dd829e779
Anton Kuleyev <kuleyev@yandex.ru>
2023-03-13 15:30:57 +0400

    Добавлен базовый функционал: лейбл счетчика, кнопки увеличения и уменьшения значения, логика их работы.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.lineBreakMode = .byClipping
+        counterLabel.text = String(count)
     }
 
-
+    @IBAction func plusTap(_ sender: Any) {
+        count += 1
+        counterLabel.text = String(count)
+    }
+    
+    @IBAction func minusTap(_ sender: Any) {
+        count = count > 0 ? count-1 : 0
+        counterLabel.text = String(count)
+    }
+    
 }
 
